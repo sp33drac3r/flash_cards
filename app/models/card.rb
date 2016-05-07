7680eea1482e7b0bc9ebb694cb3324cb90710dbc
Riley Scheid <riley.k.scheid@gmail.com>
2016-05-07 11:50:50 -0700

    Add validations to the model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,7 @@
 class Card < ActiveRecord::Base
   belongs_to :deck
+
+  validates :question, presence: true
+  validates :answer, presence: true
+  validates :deck_id, presence: true
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,4 +2,7 @@ class Deck < ActiveRecord::Base
   has_many :games
   has_many :users, through: :games
   has_many :cards
+
+  validates :name, presence: true
+
 end
