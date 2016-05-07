31006c8b2fe67f1420c7e3cb7298e2403f5d822a
Jonathan Nicolas & Kelson Adams <pair+jonathanNicolas.kelsonic@devbootcamp.com>
2016-05-07 15:05:15 -0700

    Create logins view and routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,5 +3,31 @@ get '/' do
 end
 
 get '/login' do
+  @error = params[:error]
   erb :login
 end
+
+post '/login' do
+  if session[:id]
+    redirect '/'
+  else
+    email = params[:email]
+    password = params[:password]
+    new_user = User.find_by(email: email)
+    if new_user && new_user.authenticate(password)
+      session[:id] = new_user.id
+      redirect '/'
+    else
+      # FIX THIS ERROR MESSAGE
+      redirect '/login?error=Wrong%20combination%20of%20user%20and%20password'
+    end
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
+end
+
+
+
